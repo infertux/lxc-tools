dd361a5253ee9b53a1aeff8d2bffd2c16d9ad764
Cédric Félizard <cedric@felizard.fr>
2016-01-26 22:48:54 +0000

    Update copyright year

--- lxc-start-all.sh
+++ lxc-start-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # LXC tools
-# Copyright (C) 2012 Infertux <infertux@infertux.com>
+# Copyright (C) 2012-2016 Infertux <infertux@infertux.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
